24b2b9dd892e9d6c6eb7c6015d516debacfc0564
martinma425 <martinma425@gmail.com>
2023-04-02 13:19:11 -0400

    Add marker drag&drop and visibility toggle

--- UnityProjects/RISD-ROVER-2023/Assets/CustomScripts/MapController.cs
+++ UnityProjects/RISD-ROVER-2023/Assets/CustomScripts/MapController.cs
@@ -8,6 +8,9 @@ using Microsoft.MixedReality.Toolkit.UX;
 
 namespace Microsoft.MixedReality.Toolkit
 {
+    using System;
+    using UnityEngine.UI;
+
     public class MapController : MRTKBaseInteractable
     {
         enum MapFocusMode
@@ -34,10 +37,32 @@ namespace Microsoft.MixedReality.Toolkit
         private MapFocusMode _focusMode = MapFocusMode.MapNoFocus;
         private float _mapLastRotZDeg = 0.0f;
 
+        // Marker
+        private Vector2 lastTouchPosition;
+        private bool _editMarkerMode = false;
+        [SerializeField] private GameObject markerPrefab;
+        [SerializeField] private GameObject compassMarkerPrefab;
+        [SerializeField] private float markerEditSensitivity = 0.033f;
+        private Dictionary<GameObject, (Vector3, GameObject, GameObject)> _markers;
+        private RectTransform _compassRT, _compassMarkersRT;
+        private GameObject _newMarkerOnMap, _newMarkerOnCompass;
+        private Transform _markersTF;
+        private float _panelXBound, _panelYBound;
+        private GameObject _markersObj;
+        private float _buttonPressedTime;
+
         void Start()
         {
             _mapRT = GameObject.Find("Map").GetComponent<RectTransform>();
             _meshBC = GameObject.Find("Map Panel").GetComponent<BoxCollider>();
+            _markers = new Dictionary<GameObject, (Vector3, GameObject, GameObject)>();
+            _compassRT = GameObject.Find("Compass Image").GetComponent<RectTransform>();
+            _compassMarkersRT = GameObject.Find("Compass Markers").GetComponent<RectTransform>();
+            _markersObj = GameObject.Find("Markers");
+            _markersTF = _markersObj.GetComponent<Transform>();
+            var panelSize = GameObject.Find("Map Panel").GetComponent<BoxCollider>().size;
+            _panelXBound = panelSize.x / 2;
+            _panelYBound = panelSize.y / 2;
         }
 
         void Update()
@@ -51,6 +76,7 @@ namespace Microsoft.MixedReality.Toolkit
                     AlignMapWithUser();
                     break;
             }
+            if (_markers.Count > 0) UpdateMarkers();
         }
 
         public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
@@ -76,12 +102,51 @@ namespace Microsoft.MixedReality.Toolkit
                             // Focus
                             _focusMode = MapFocusMode.MapNoFocus;
                             Debug.Log(_focusMode);
+
+                            // Marker
+                            if (_editMarkerMode)
+                            {
+                                _newMarkerOnMap = Instantiate(markerPrefab, _markersTF);
+                                _newMarkerOnCompass = Instantiate(compassMarkerPrefab, _compassMarkersRT);
+                                _newMarkerOnMap.GetComponent<RawImage>().color = new Color(1, 1, 1, 0.5f);
+                                _newMarkerOnCompass.GetComponent<RawImage>().color = new Color(1, 1, 1, 0.5f);
+                                _markers.Add(_newMarkerOnMap, (MapToWorldPos(firstPosition), _newMarkerOnMap, _newMarkerOnCompass));
+                            }
+                            else
+                            {
+                                float minDist = markerEditSensitivity + 1;
+                                foreach (var kvp in _markers)
+                                {
+                                    float dist = (kvp.Value.Item1 - MapToWorldPos(firstPosition)).magnitude;
+                                    if (dist < minDist)
+                                    {
+                                        minDist = dist;
+                                        _newMarkerOnMap = kvp.Key;
+                                        _newMarkerOnCompass = kvp.Value.Item3;
+                                    }
+                                }
+
+                                if (minDist < markerEditSensitivity)
+                                {
+                                    _editMarkerMode = true;
+                                }
+                            }
                         }
 
                         // Update the offsets (top, right, bottom, left) based on the change in position
                         Vector2 delta = localTouchPosition - firstPosition;
-                        _mapRT.offsetMin = initialOffsetMin + delta;
-                        _mapRT.offsetMax = _mapRT.offsetMin;
+
+                        if (_editMarkerMode)
+                        {
+                            var markerItem = _markers[_newMarkerOnMap];
+                            markerItem.Item1 = MapToWorldPos(localTouchPosition);
+                            _markers[_newMarkerOnMap] = markerItem;
+                        }
+                        else
+                        {
+                            _mapRT.offsetMin = initialOffsetMin + delta;
+                            _mapRT.offsetMax = _mapRT.offsetMin;
+                        }
 
                         // Write/update the last-position
                         if (lastPositions.ContainsKey(interactor))
@@ -92,6 +157,8 @@ namespace Microsoft.MixedReality.Toolkit
                         {
                             lastPositions.Add(interactor, localTouchPosition);
                         }
+
+                        lastTouchPosition = localTouchPosition;
 
                         break;
                     }
@@ -189,9 +256,110 @@ namespace Microsoft.MixedReality.Toolkit
         {
             base.OnSelectExited(args);
 
+            if (_editMarkerMode)
+            {
+                // Discard marker if being "thrown out" of the map
+                if (Math.Abs(lastTouchPosition.x) > _panelXBound ||
+                    Math.Abs(lastTouchPosition.y) > _panelYBound)
+                {
+                    _markers.Remove(_newMarkerOnMap);
+                    Destroy(_newMarkerOnMap);
+                    Destroy(_newMarkerOnCompass);
+                }
+                else
+                {
+                    _newMarkerOnMap.GetComponent<RawImage>().color = new Color(1, 1, 1, 1);
+                    _newMarkerOnCompass.GetComponent<RawImage>().color = new Color(1, 1, 1, 1);
+                }
+
+                _editMarkerMode = false;
+            }
+
             // Remove the interactor from our last-position collection when it leaves.
             lastPositions.Remove(args.interactorObject);
         }
 
+        /************* Marker **************/
+        public void OnMarkerButtonSelectEnter()
+        {
+            _buttonPressedTime = Time.time;
+        }
+
+        public void OnMarkerButtonSelectExit()
+        {
+            float delta = Time.time - _buttonPressedTime;
+            if (delta > 1f)
+            {
+                _editMarkerMode = true;
+            }
+            else
+            { ;
+                _markersObj.SetActive(!_markersObj.activeSelf);
+            }
+        }
+
+        private Vector3 MapToWorldPos(Vector2 mapPos)
+        {
+            float scaleW2M = 100.0f * _mapRT.localScale.x;
+            float mapRotZDeg = _mapRT.localEulerAngles.z;
+            Vector2 mapOffset = _mapRT.offsetMin;
+            Vector3 worldPos = new Vector3(mapPos.x - mapOffset.x, 0, mapPos.y - mapOffset.y);
+
+            // Unrotate then scale to obtain the world space position
+            worldPos = Quaternion.Euler(0.0f, mapRotZDeg, 0.0f) * worldPos;
+            worldPos /= scaleW2M;
+
+            return worldPos;
+        }
+
+        private Vector2 WorldToMapPos(Vector3 worldPos)
+        {
+            float scaleW2M = 100.0f * _mapRT.localScale.x;
+            float mapRotZDeg = _mapRT.localEulerAngles.z;
+
+            // Rotate then scale to obtain the map space position
+            Vector3 mapPos = Quaternion.Euler(0.0f, -mapRotZDeg, 0.0f) * worldPos;
+            mapPos *= scaleW2M;
+
+            return new Vector2(mapPos.x, mapPos.z);
+        }
+
+        private void UpdateMarkers()
+        {
+            float scaleW2M = 100.0f * _mapRT.localScale.x;
+            float mapRotZDeg = _mapRT.localEulerAngles.z;
+            float compassWidth = _compassRT.rect.width / 360.0f;
+
+            Vector3 userPos = Camera.main.transform.position;
+            Vector3 userLook = Camera.main.transform.forward;
+            userLook.y = 0.0f;
+
+            foreach(var kvp in _markers)
+            {
+                Vector3 posWorldspace = kvp.Value.Item1;    // mark pos in world space
+                RectTransform rtMap = kvp.Value.Item2.GetComponent<RectTransform>();
+                RectTransform rtCompass = kvp.Value.Item3.GetComponent<RectTransform>();
+
+                // Translate (offset) markers relative to map RT
+                // Note: pos gives offsets in rotated MAP SPACE,
+                //       but we must compute offsets in PANEL SPACE
+
+                // Marker pos in map space, with rotation of map (xz components)
+                Vector3 posMapspace = posWorldspace * scaleW2M;
+                // Rotate posMapspace back to get coords in unrotated map coords (xz components)
+                Vector3 posMapspaceUnrot = Quaternion.Euler(0.0f, -mapRotZDeg, 0.0f) * posMapspace;
+
+                rtMap.offsetMin = _mapRT.offsetMin + new Vector2(posMapspaceUnrot.x, posMapspaceUnrot.z);
+                rtMap.offsetMax = rtMap.offsetMin;
+
+                // Adjust marker position on compass
+                // 1. Get relative angle of marker from front in range -180 ~ 180
+                Vector3 markerDir = posWorldspace - userPos;
+                markerDir.y = 0.0f;
+                float angleToMarker = -Vector3.SignedAngle(markerDir, userLook, Vector3.up);
+                rtCompass.offsetMin = new Vector2(angleToMarker * compassWidth, 0.0f);
+                rtCompass.offsetMax = rtCompass.offsetMin;
+            }
+        }
     }
 }
