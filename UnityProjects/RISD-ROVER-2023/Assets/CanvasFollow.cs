44942fddd6b11629b630bf5b56b3953d2f95703a
martinma425 <martinma425@gmail.com>
2023-04-25 19:12:34 -0400

    Fix canvas follow logic

---
+++ UnityProjects/RISD-ROVER-2023/Assets/CanvasFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasFollow : MonoBehaviour
+{
+    // Canvas follow
+    [SerializeField] private float distanceFromUser = 0.5f;
+    private Transform _canvasTf;
+    private Camera _mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _canvasTf = GameObject.Find("Canvas").GetComponent<RectTransform>().transform;
+        _mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        Transform cameraTf = _mainCamera.transform;
+        Vector3 userPos = cameraTf.position;
+        Vector3 userLook = cameraTf.forward;
+
+        userLook.y = 0;
+        userLook = Vector3.Normalize(userLook);
+
+        _canvasTf.position = userPos + distanceFromUser * userLook;
+        _canvasTf.rotation = cameraTf.rotation;
+    }
+}
